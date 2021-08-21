42e024e199e1d7da926d7bf32b646496b14576cc
Renato <renato.szk@hotmail.com>
2021-08-21 14:53:00 +0300

    metodo 1 com URL.createObject(file)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [images, setImages] = useState([])
+
+  const dragEvents = { 
+    onDragEnter: (e)=> { 
+      e.preventDefault();
+      console.log('onDragEnter');
+    },
+    onDragLeave: (e)=> { 
+      e.preventDefault();
+      console.log('onDragLeave');
+    },
+    onDragOver: (e)=> { 
+      e.preventDefault();
+      console.log('onDragOver');
+    },
+    onDrop: (e)=> { 
+      e.preventDefault();
+      console.log('onDrop');
+
+      const files = Array.from(e.dataTransfer.files);
+      const images = files.map((file)=>{
+        const {name,size} = file;
+        return {
+          name,
+          size, 
+          preview: URL.createObjectURL(file)
+        }
+      });
+      setImages(images)
+    },
+  }
+  console.log(images)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='logo'>
+        <img src='/photography-group.png' alt='Photography Group' />
+      </div>
+      <div className='file-drop'  {...dragEvents} >
+        <div className='text'>Drag & Drop your Image here.</div>
+      </div>
+      <div className='preview'>
+        {images.map((image)=>{
+          return (
+          <div className='image' key={image.name}>
+            <img src={image.preview} alt={image.name} />
+          </div>)
+        })}
+
+
+        {/* <div className='image'><img src="https://loremflickr.com/600/600?random=1" alt='' /></div>
+        <div className='image'><img src="https://loremflickr.com/600/600?random=2" alt='' /></div>
+        <div className='image'><img src="https://loremflickr.com/600/600?random=3" alt='' /></div>
+        <div className='image'><img src="https://loremflickr.com/600/600?random=4" alt='' /></div>
+        <div className='image'><img src="https://loremflickr.com/600/600?random=5" alt='' /></div>
+        <div className='image'><img src="https://loremflickr.com/600/600?random=6" alt='' /></div>
+        <div className='image'><img src="https://loremflickr.com/600/600?random=7" alt='' /></div>
+        <div className='image'><img src="https://loremflickr.com/600/600?random=8" alt='' /></div> */}
+
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
